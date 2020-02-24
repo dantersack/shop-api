c1be84738464cf10a6fae0177e5b943416b3e390
dante <sack_dante@hotmail.com>
2020-02-24 19:13:27 -0300

    Mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,40 +1,91 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 // GET
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products"
-  });
+  Product.find()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // POST
 router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "Handling POST requests to /products"
+  const product = new Product({
+    _id: mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // GET {id}
 router.get("/:productID", (req, res, next) => {
   const id = req.params.productID;
-  res.status(200).json({
-    message: `The product ID is: ${id}`
-  });
+  Product.findById(id)
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
-// PATCH
+// PATCH {id}
 router.patch("/:productID", (req, res, next) => {
-  res.status(200).json({
-    message: `Product updated.`
-  });
+  Product.findByIdAndUpdate(req.params.productID, {
+    name: req.body.name,
+    price: req.body.price
+  })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Product updated" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
-// DELETE
+// DELETE {id}
 router.delete("/:productID", (req, res, next) => {
-  res.status(200).json({
-    message: `Product deleted.`
-  });
+  Product.deleteOne({ _id: req.params.productID })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Product deleted" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,66 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
-// next moves the request to the next middleware in line
+// DB Connection
+mongoose
+  .connect(
+    `mongodb+srv://dante:${process.env.MONGO_ATLAS_PASSWORD}@shop-api-cluster-qvlun.mongodb.net/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(result =>
+    console.log(
+      `MongoDB Connected to db=${result.connections[0].name} host=${result.connections[0].host} `
+    )
+  )
+  .catch(err => console.log(err));
+
+/*
+ * Middlewares
+ */
+
+// Logger Middleware
+app.use(morgan("dev"));
+
+// Body-Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handling CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // '*' gives access to any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+// Handling Errors
+app.use((req, res, next) => {
+  const error = new Error("404 - Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: { message: err.message }
+  });
+});
 
 module.exports = app;
